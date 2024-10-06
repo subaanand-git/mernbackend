850be33443b005e5d6cec72e263125618334fb0f
subaanand-git <subaanand1070@gmail.com>
2024-10-06 18:10:46 +0530

    updated

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto')
 
