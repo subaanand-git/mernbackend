7665564a9e3a2c0f6b31713c00c87c5ba8ebeffe
subaanand-git <subaanand1070@gmail.com>
2024-10-06 17:29:58 +0530

    updated

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use('/api/v1/',order);
 app.use('/api/v1/',payment);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, './frontend/build')));
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+        res.sendFile(path.resolve(__dirname, './frontend/build/index.html'))
     })
 }
 
