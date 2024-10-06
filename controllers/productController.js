945b14302de251c33ec6ede0f96c7f07ab0e9830
subaanand-git <subaanand1070@gmail.com>
2024-10-06 21:44:50 +0530

    success

--- controllers/productController.js
+++ controllers/productController.js
@@ -115,7 +115,7 @@ exports.updateProduct = catchAsyncError(async (req, res, next) => {
 
 //Delete Product - api/v1/product/:id
 exports.deleteProduct = catchAsyncError(async (req, res, next) =>{
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findByIdAndDelete(req.params.id);
 
     if(!product) {
         return res.status(404).json({
